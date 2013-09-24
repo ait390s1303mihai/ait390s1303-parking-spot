9628d2bac6a487bca57dd8ab42bdbe3723d456e9
ait390s1301minseop <ait390s1301minseop@gmail.com>
2013-09-23 20:23:56 -0400

    Added stylesheets folder in war/ Added main.css in war/stylesheets

--- ParkingSpotGIT/ParkingSpot/src/parkingspot/ParkingSpotServlet.java
+++ ParkingSpotGIT/ParkingSpot/src/parkingspot/ParkingSpotServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.*;
  */
 @SuppressWarnings("serial")
 public class ParkingSpotServlet extends HttpServlet {
+	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
