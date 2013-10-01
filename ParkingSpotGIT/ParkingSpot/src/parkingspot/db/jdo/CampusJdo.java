e747af62b481e58a2e0abdcc215ce4fdcbba9f71
Alex Leone <ait390s1303alex@gmail.com>
2013-09-30 20:26:46 -0400

    added the jdo class templates

---
+++ ParkingSpotGIT/ParkingSpot/src/parkingspot/db/jdo/CampusJdo.java
@@ -0,0 +1,24 @@
+package parkingspot.db.jdo;
+
+/**
+ * 
+ *	ENTITY KIND: "Campus"
+ *	PARENT: NONE
+ *	KEY: A campus Id
+ *	FEATURES:
+ *		Name: "Id" Type: int
+ *		Name: "Name" Type: String
+ *		Name: "Address" Type: String
+ *		Name: "Location"Type: String
+ *	Examples:
+ *	Campus("Fairfax")
+ *		"Id" = 1
+ *		"Address" = "4400 University Dr., Fairfax, VA 22030, USA"
+ *		"Location" =  "United States@38.826182,-77.308211"
+ *		"Name" = "Fairfax Campus"
+ *  
+ */     
+public class CampusJdo {
+	
+
+}
---
+++ ParkingSpotGIT/ParkingSpot/src/parkingspot/db/jdo/PermitJdo.java
@@ -0,0 +1,23 @@
+package parkingspot.db.jdo;
+
+/**
+ * 
+ *	ENTITY KIND: "Permit"
+ * 	PARENT: NONE
+ *	KEY: Permit Type ID (int)
+ *	FEATURES:
+ *		Name: "Permit Id" Type: Int
+ *		Name: "Permit_Name" Type: String
+ *		Name: "Fuel_Efficient" Type:Boolean
+ *	Examples:
+ *	Permit("Teacher")
+ *		"Permit ID" = 1
+ *		"Permit_Name" = "Faculty and Workers"
+ *		"Fuel_Efficient" ="True"
+ *  
+ */ 
+
+public class PermitJdo {
+	
+	
+}
