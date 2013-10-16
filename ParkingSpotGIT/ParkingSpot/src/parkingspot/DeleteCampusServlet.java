c63cc06242ed43473ce6ca22690c0fb810132d0c
Mihai B. <ait390s1303mihai@gmail.com>
2013-10-16 11:04:21 -0400

    warnings

--- ParkingSpotGIT/ParkingSpot/src/parkingspot/DeleteCampusServlet.java
+++ ParkingSpotGIT/ParkingSpot/src/parkingspot/DeleteCampusServlet.java
@@ -17,22 +17,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import parkingspot.db.Campus;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-//import com.google.appengine.api.datastore.Key;
-//import com.google.appengine.api.datastore.Key;
-//import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.PreparedQuery.TooManyResultsException;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.FilterOperator;
-
-
 //TOOD comments
 
 @SuppressWarnings("serial")
 public class DeleteCampusServlet extends HttpServlet {
-	@SuppressWarnings("deprecation")
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		Campus.deleteCampusCommand(req.getParameter("campusID"));
